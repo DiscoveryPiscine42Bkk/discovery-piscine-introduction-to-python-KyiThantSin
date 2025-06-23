92042b53fed4180d233f7204687aff8a7db7b8ea
KyiThantSin <kyits485@gmail.com>
2025-06-23 23:21:42 +0700

    cell03 ex02

---
+++ cell03/ex02/i_got_that.py
@@ -0,0 +1,7 @@
+user_input = input("What you gotta say? : ")
+
+while True:
+    if user_input == "STOP":
+        break
+    print("I got that! Anything else? : ", user_input)
+    user_input = input("I got that! Anything else? : ")
