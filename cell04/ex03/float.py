64ce601dbc3ac7b675e6df9806e017e7f662055c
KyiThantSin <kyits485@gmail.com>
2025-06-24 11:27:26 +0700

    cell04 ex03

---
+++ cell04/ex03/float.py
@@ -0,0 +1,11 @@
+num = input("Give me a number: ")
+
+try:
+    number = float(num)
+    if number == int(number):
+        print("This number is an integer.")
+    else:
+        print("This number is a decimal.")
+except ValueError:
+    print("That's not a valid number.")
+
