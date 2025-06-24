fc76cf0c7b22aec7373cce91e4163025c6f0d261
KyiThantSin <kyits485@gmail.com>
2025-06-25 01:46:54 +0700

    cell05 ex03

--- cell05/ex03/play_with_arrays.py
+++ cell05/ex03/play_with_arrays.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 
-original_array = [2, 8, 9, 48, 8, 22, -12, 2]
+original_array = [2, 4, 9, 48, 8, 22, -12, 2]
 
-new_array = {num + 2 for num in original_array if (num + 2) > 5}
+new_array = {num + 2 for num in original_array if (num) > 5}
 
 print(original_array)
 print(new_array)
